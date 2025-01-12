562fd408ce480f7a610959436d7fe0c7fe918172
Renan <schlemperdev@gmail.com>
2025-01-12 20:56:57 -0300

    up

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Admin\SupportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('supports.index');
 });
 
 Route::get('/contact', [SiteController::class, 'contact']);
